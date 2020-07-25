956963f7a9521cbdf3333fd9cadb20eb76f7662b
joyantoBhakto <joyanto72bubt@gmail.com>
2020-07-25 14:45:49 +0600

    update

---
+++ src/Table/tableHookUtils.ts
@@ -0,0 +1,13 @@
+
+
+export function sum(arr: any[]) {
+  return arr.reduce((prev, curr) => prev + curr, 0)
+}
+
+export function getFirstDefined(...args: any[]) {
+  for (let i = 0; i < args.length; i += 1) {
+    if (typeof args[i] !== 'undefined') {
+      return args[i]
+    }
+  }
+}
